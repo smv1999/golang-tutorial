b40bc11385474c5d49684ede4cf82e1dd35951f5
Vaidhyanathan S M <vaidhyanathan.sm@gmail.com>
2021-06-10 00:10:48 +0530

    Done with formatted_strings

---
+++ formatted_strings.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 21
+	name := "Vaidhyanathan S M"
+	// %v - default format, %q - quotes, %T - type of
+	fmt.Printf("my age is %d and my name is %s \n", age, name)
+	fmt.Printf("my age is %v and my name is %q \n", age, name)
+	fmt.Printf("age is of type %T \n", age)
+	fmt.Printf("you scored %.2f points \n", 225.55)
+
+	// Sprintf - save formatted strings
+	var str = fmt.Sprintf("my age is %v and my name is %v \n", age, name)
+	fmt.Println("The saved string is ", str)
+}
